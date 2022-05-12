c3c0027fd85d3eaadc1d7d6ed0bf8366a26275a6
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-05-12 16:34:54 +0200

    set up sampling logistics for similar pairs expt

---
+++ code/src/reference_game_train.py
@@ -0,0 +1,2 @@
+# next to drift metrics, track somehow how the targets and distractors are paired
+# i.e., track ther (dis)similarity, to be able to make to caption granularity comparison to the second experiment
