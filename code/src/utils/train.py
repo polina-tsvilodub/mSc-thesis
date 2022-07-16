92af85d3da5f649b5c6312bb19a2892b42b0dcad
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-07-16 23:29:33 +0200

    final S pretraining on coco params

--- code/src/utils/train.py
+++ code/src/utils/train.py
@@ -4,6 +4,7 @@ import math
 import torch
 import numpy as np
 import pandas as pd
+from random import shuffle
 from . import early_stopping
 import torch.nn as nn 
 from drift_metrics import metrics
@@ -11,21 +12,31 @@ import random
 
 def validate_model(
     data_loader_val,
-    encoder,
     decoder,
 ):
     """
     Utility function for computing the validation performance of the speaker model while pre-training.
+    
+    Parameters:
+    ---------
+    data_loader_val:
+        Validation data loader for images not used during training.
+    decoder: 
+        Trained decoder RNN to be evaluated.
+
+    Returns:
+    -------
+    val_loss: float
+        Average validation loss over the evaluation images.
     """
     # init params
     val_running_loss = 0.0
     counter = 0
     embedded_imgs = torch.load("./notebooks/resnet_pretrain_embedded_val_imgs.pt")
-    total_steps = math.ceil(len(data_loader_val.dataset.caption_lengths) / data_loader_val.batch_sampler.batch_size)
+    total_steps = math.floor(len(data_loader_val.dataset.caption_lengths) / data_loader_val.batch_sampler.batch_size)
     print("Val total steps: ", total_steps+1)
     criterion = nn.CrossEntropyLoss().cuda() if torch.cuda.is_available() else nn.CrossEntropyLoss()
 
-    encoder.eval()
     decoder.eval()
 
     # evaluate model on the batch
@@ -33,18 +44,14 @@ def validate_model(
         print("Validating the model...")
         for i in range(1, total_steps+1): 
              # get val indices
-            indices = data_loader_val.dataset.get_func_train_indices()
-            # create separate lists for retrieving the image emebddings
-            # target_inds = torch.tensor([x[0] for x in indices]).long()
-            # distractor_inds = torch.tensor([x[1] for x in indices]).long()
+            indices = data_loader_val.dataset.get_func_train_indices(i)
 
             new_sampler = torch.utils.data.sampler.SubsetRandomSampler(indices=indices)
             data_loader_val.batch_sampler.sampler = new_sampler
 
             counter += 1
-            # print("Counter: ", counter)
             # Obtain the batch.
-            targets, distractors, target_captions = next(iter(data_loader_val))
+            targets, distractors, target_features, distractor_features, target_captions, distractor_captions = next(iter(data_loader_val))
 
             # Move batch of images and captions to GPU if CUDA is available.
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -52,15 +59,7 @@ def validate_model(
             distractors = distractors.to(device)
             target_captions = target_captions.to(device)
 
-            # Pass the inputs through the CNN-RNN model.
-            # after retrieving the resnet embeddings
-            # target_features = torch.index_select(embedded_imgs, 0, target_inds)
-            # distractor_features = torch.index_select(embedded_imgs, 0, distractor_inds)
-
             # compute val predictions and loss
-            target_features = encoder(targets)#encoder(target_features)
-            distractor_features = encoder(distractors) #encoder(distractor_features)
-
             both_images = torch.cat((target_features, distractor_features), dim=-1)
             outputs = decoder(both_images, target_captions)
             
@@ -71,11 +70,11 @@ def validate_model(
             loss = criterion(outputs.contiguous().view(-1, vocab_size), target_captions[:, 1:].reshape(-1))  
 
             val_running_loss += loss.item()
-            # print("val running loss: ", val_running_loss)
             
         
         val_loss = val_running_loss / counter
-        
+        print("Val loss: ", val_loss)
+            
         return val_loss
 
 
@@ -85,7 +84,6 @@ def pretrain_speaker(
     total_steps,
     data_loader,
     data_loader_val, 
-    # encoder,
     decoder,
     params,
     criterion,
@@ -107,8 +105,8 @@ def pretrain_speaker(
             Number of steps per epoch, calculated from batch size as dataset size.
         data_loader: torch.DataLoader
             Data loader batching the img caption pairs.
-        encoder: CNN
-            Pretrained ResNet-50 instance.
+        data_loader_val: torch.DataLoader
+            Data loader batching the img caption pairs for validation (separate val images).
         decoder: LSTM
             LSTM decoder instance.
         params: list
@@ -127,9 +125,9 @@ def pretrain_speaker(
 
     # Open the training log file.
     f = open(log_file, 'w')
-    csv_out = "../../data/pretrain_coco_512dim_teacher_forcing_scheduled_desc_05_byEp_pureDecoding_losses_padding_"
-    val_csv_out = "../../data/pretrain_coco_512dim_teacher_forcing_scheduled_desc_05_byEp_pureDecoding_val_padding_"
-    csv_metrics = "pretrain_coco_512dim_teacher_forcing_scheduled_desc_05_byEp_pureDecoding_metrics_padding_"
+    csv_out = "../../data/final/losses_final_pretrained_speaker_coco_4000vocab_tf_desc05_padding_pureDecoding_"
+    val_csv_out = "../../data/final/val_final_pretrained_speaker_coco_4000vocab_tf_desc05_padding_pureDecoding_"
+    csv_metrics = "../../data/final/metrics_final_pretrained_speaker_coco_4000vocab_tf_desc05_padding_pureDecoding_"
     
     speaker_losses=[]
     perplexities = []
@@ -145,6 +143,7 @@ def pretrain_speaker(
     cont_overlaps = []
     train_type = []
     forcing_rate = []
+    sanity_check_inds = []
 
     # init the early stopper
     early_stopper = early_stopping.EarlyStopping(patience=3, min_delta=0.03)
@@ -155,7 +154,6 @@ def pretrain_speaker(
     # the higher the number, the longer teacher focing will be used
     k = 150
 
-    # embedded_imgs = torch.load("3dshapes_all_ResNet_features_reshaped_all.pt") #torch.cat(( torch.load("3dshapes_all_ResNet_features_reshaped_23000_first.pt"), torch.load("3dshapes_all_ResNet_features_reshaped_240000_first.pt") ), dim = 0) #torch.load("COCO_train_ResNet_features_reshaped.pt")
     # init drift meter for tracking structural drift for reference
     drift_meter = metrics.DriftMeter(
         # semantic_encoder="models/encoder-earlystoppiing-4_semantic-drift.pkl", 
@@ -164,33 +162,29 @@ def pretrain_speaker(
         embed_size=512, 
         vis_embed_size=512, 
         hidden_size=512,
-        # TODO this will have to be retrained
         vocab=len(data_loader.dataset.vocab)
     )
     softmax = nn.Softmax(dim=-1)
-    
-    for epoch in range(1, num_epochs+1):
+    # create a list of "steps" which can be shuffled as a proxy for shuffling the indices of the images used for batching
+    steps_nums = list(range(1, total_steps+1))
 
-        for i_step in range(1, total_steps+1):
+    for epoch in range(1, num_epochs+1):
+        shuffle(steps_nums)
+        for i, i_step in enumerate(steps_nums):
             decoder.train()
             # set models into training mode
             hidden = decoder.init_hidden(batch_size=data_loader.batch_sampler.batch_size)
                     
             # Randomly sample a caption length, and sample indices with that length.
-            indices = data_loader.dataset.get_func_train_indices()
-            # create separate lists for retrieving the image emebddings
-            # target_inds = torch.tensor([x[0] for x in indices]).long()
-            # distractor_inds = torch.tensor([x[1] for x in indices]).long()
-            # print("Indices targets: ", target_inds)
-            # print("Indices distractors: ", distractor_inds)
-            # print("Indices: ", indices)
+            indices = data_loader.dataset.get_func_train_indices(i_step)
+            sanity_check_inds.append(indices)
             # Create and assign a batch sampler to retrieve a batch with the sampled indices.
             new_sampler = torch.utils.data.sampler.SubsetRandomSampler(indices=indices)
             data_loader.batch_sampler.sampler = new_sampler
             
             # Obtain the batch.
             targets, distractors, target_features, distractor_features, target_captions, distractor_captions = next(iter(data_loader)) 
-            
+                
             # Move batch of images and captions to GPU if CUDA is available.
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
             targets = targets.to(device)
@@ -199,24 +193,13 @@ def pretrain_speaker(
             
             # Zero the gradients (reset).
             decoder.zero_grad()
-            # encoder.zero_grad()
             
-            # Pass the inputs through the CNN-RNN model.
-            # after retrieving the resnet embeddings
-            # target_features = torch.index_select(embedded_imgs, 0, target_inds)
-            # distractor_features = torch.index_select(embedded_imgs, 0, distractor_inds)
-
-            # target_features = encoder(targets)#encoder(target_features)
-            # print("Script encoder output: ", target_features)
-            # distractor_features = encoder(distractors) #encoder(distractor_features)
-            # print("embedded resnet features shape: ", target_features.shape)
             # concat image features
-            both_images = torch.cat((target_features.unsqueeze(1), distractor_features.unsqueeze(1)), dim=1) # [target_features, distractor_features]
+            both_images = torch.cat((target_features.unsqueeze(1), distractor_features.unsqueeze(1)), dim=1) 
             
             ### scheduled sampling 
             if use_teacher_forcing_rate == "scheduled":
                 p = (k / (k + np.exp( ((epoch - 1) * total_steps + i_step) / k)))
-                # print(((num_epochs - 1) * total_steps + i_step))
                 print("P : ", p)
                 forcing_rate.append(p)
                 train_type.append("scheduled_sampling")
@@ -227,10 +210,9 @@ def pretrain_speaker(
                 
                 # create initial caption input: "START"
                 cat_samples = torch.tensor([0, 0]).repeat(data_loader.batch_sampler.batch_size, 1)
-                # print("target captions: ", target_captions.shape)
-                # print(target_captions[0])
                 for i in range(target_captions.shape[-1]-1):
                     out, hidden_state = decoder.forward(both_images, cat_samples, init_hiddens)
+                    # for exp sampling
                     # out = out ** 5
                     outputs.append(out)
                     probs = softmax(out)
@@ -239,12 +221,10 @@ def pretrain_speaker(
                     if np.random.choice([True, False], 1, p = [p, 1-p]):
                         
                         cat_samples = target_captions[:, i+1].unsqueeze(1) # i + 1 to skip start token 
-                        # print(cat_samples.shape)
-                        # print("appended: ", target_captions[0, i+1])
                     # auto-regression option
                     else:
                        
-                        # only do pure sampling for now
+                        # do pure sampling
                         # cat_dist = torch.distributions.categorical.Categorical(probs)
                         # greedy
                         _, cat_samples = torch.max(probs, dim = -1) #cat_dist.sample()
@@ -276,16 +256,16 @@ def pretrain_speaker(
                 #     start_caption = torch.cat((pred_ind, pred_ind), dim = -1)
                 #     print("Duplicated predicted index: ", start_caption.shape)
                     max_seq_length = target_captions.shape[1]-1
-                    captions_pred, log_probs, outputs, entropies = decoder.sample(both_images, max_sequence_length=max_seq_length, decoding_strategy="pure")
-                # print("FINAL SHAPE of SAMPLED: ", outputs.shape)
+                    captions_pred, log_probs, outputs, entropies = decoder.sample(
+                        both_images, 
+                        max_sequence_length=max_seq_length, 
+                        decoding_strategy="pure"
+                    )
             # The size of the vocabulary.
             vocab_size = len(data_loader.dataset.vocab)
 
             # Calculate the batch loss.
-            # print("Outputs shape for loss: ", outputs.shape)
-            # print("transposed output: ", outputs.transpose(1,2).shape)
-            # print("Targets shape for loss: ", target_captions.shape)
-            loss = criterion(outputs.transpose(1,2), target_captions[:, 1:]) # 
+            loss = criterion(outputs.transpose(1,2), target_captions[:, 1:]) 
             # print("Loss: ", loss)
             # Backward pass.
             loss.backward()
@@ -302,7 +282,6 @@ def pretrain_speaker(
         
             # Print training statistics (on same line).
             print('\r' + stats, end="")
-            # print(loss.item())
             sys.stdout.flush()
             
             # Print training statistics to file.
@@ -310,7 +289,7 @@ def pretrain_speaker(
             f.flush()
             
             # Print training statistics (on different line).
-            if i_step % 500 == 0:
+            if i % 500 == 0:
                 print('\r' + stats)
                 decoder.eval()
                 init_hidden = decoder.init_hidden(1)
@@ -322,15 +301,6 @@ def pretrain_speaker(
                     outputs_ind = softmax(outputs[i])
                     max_probs, cat_samples = torch.max(outputs_ind, dim = -1)
 
-                    # TODO check masking tokens after end
-                    # semantic_drift = drift_meter.semantic_drift(cat_samples, both_images[i])
-                    # semantic_drifts.append(semantic_drift)
-                    # print("----- Semantic drift ---- ", semantic_drift)
-                    # for comparison, compute semantic drift of griund truth caption
-                    # semantic_drift_true = drift_meter.semantic_drift(target_captions[i], both_images[i])
-                    # semantic_drifts_true.append(semantic_drift_true)
-                    # print("----- Semantic drift ground truth ---- ", semantic_drift_true)
-                    
                     nl_caption = [data_loader.dataset.vocab.idx2word[w.item()] for w in cat_samples]
                     nl_caption = " ".join(nl_caption)
                     structural_drift = drift_meter.structural_drift(nl_caption)
@@ -342,52 +312,35 @@ def pretrain_speaker(
                     structural_drift_true = drift_meter.structural_drift(nl_true_caption)
                     structural_drifts_true.append(structural_drift_true)
 
-                    # TODO overlap based drift metrics
-                    # discrete_overlap = drift_meter.compute_discrete_overlap(cat_samples, target_captions[i], distractor_captions[i])
-                    # # print("Discrete overlap score ", discrete_overlap)
-                    # discrete_overlaps.append(discrete_overlap)
-                    # # TODO get embeddings of the captions for discrete overlap computation
-                    # with torch.no_grad():
-                    #     _ , prediction_embs = decoder.forward(both_images[i].unsqueeze(0), cat_samples.unsqueeze(0), init_hidden)#decoder.embed(cat_samples)
-                    #     target_embs = hidden[0].detach() #decoder.embed(target_captions[i])
-                    #     _ , distractor_embs = decoder.forward(both_images[i].unsqueeze(0), distractor_captions[i].unsqueeze(0), init_hidden)#decoder.embed(cat_samples)
-                    # cont_overlap = drift_meter.compute_cont_overlap(prediction_embs[0], target_embs[:, i, :], distractor_embs[0])
-                    # # print("Continuous overlap ", cont_overlap)
-                    # cont_overlaps.append(cont_overlap)
-                    # compute image similarities
-                    # cos_sim_img = drift_meter.image_similarity(target_features, distractor_features)
-                    # image_similarities.append(cos_sim_img)
+                    
         # Save the weights.
         if epoch % save_every == 0:
-            torch.save(decoder.state_dict(), os.path.join('./models', 'decoder-coco-512dim-teacher_forcing_scheduled_desc_05_byEp_pureDecoding_padded-%d.pkl' % epoch))
+            torch.save(decoder.state_dict(), weights_path + '_%d.pkl' % epoch ) # os.path.join('./models', 'decoder-coco-512dim-teacher_forcing_scheduled_desc_05_byEp_pureDecoding_padded_indFixed-%d.pkl' % epoch)
             
+            torch.save(sanity_check_inds, "../../data/final/sanity_checked_dataloader_indices.pt")
             use_teacher_forcing_rate = use_teacher_forcing_rate * 0.5
             # compute validation loss
-            # with torch.no_grad():
-                # val_loss = validate_model(
-                #     data_loader_val,
-                #     encoder,
-                #     decoder,
-                # )     
-                # # torch.save(decoder.state_dict(), os.path.join('./models', 'decoder-2imgs-earlystopping-%d-step-%d.pkl' % (epoch, i_step)))
-                # # torch.save(encoder.state_dict(), os.path.join('./models', 'encoder-2imgs-earlystoppiing-%d-step-%d.pkl' % (epoch, i_step)))
+            with torch.no_grad():
+                val_loss = validate_model(
+                    data_loader_val,
+                    decoder,
+                )     
                 
-                # val_stats = 'Epoch [%d/%d], Step [%d/%d], Validation loss: %.4f' % (epoch, num_epochs, i_step, total_steps, val_loss)
-                # print(val_stats)
-                # f.write(val_stats + '\n')
-                # f.flush()   
+                val_stats = 'Epoch [%d/%d], Step [%d/%d], Validation loss: %.4f' % (epoch, num_epochs, i_step, total_steps, val_loss)
+                print(val_stats)
+                f.write(val_stats + '\n')
+                f.flush()   
                 
-                # val_losses.append(val_loss)
-                # val_steps.append(i_step)
-                # # update stooper
-                # early_stopper(val_loss)
-                # # check if we want to break
-                # if early_stopper.early_stop:
-                #     # save the models
-                #     torch.save(decoder.state_dict(), os.path.join('./models', 'decoder-byImage-prepend-1024dim-earlystopping-6000vocab-wResNet-%d.pkl' % epoch))
-                #     torch.save(encoder.state_dict(), os.path.join('./models', 'encoder-byImage-prepend-1024dim-earlystopping-6000vocab-wResNet-%d.pkl' % epoch))
-                #     print("Early stopping steps loop!")
-                #     break
+                val_losses.append(val_loss)
+                val_steps.append(i_step)
+                # update stooper
+                early_stopper(val_loss)
+                # check if we want to break
+                if early_stopper.early_stop:
+                    # save the models
+                    torch.save(decoder.state_dict(), weights_path + '_earlystopping_%d.pkl' % epoch)
+                    print("Early stopping steps loop!")
+                    break
 
         # save the training metrics
         df_out = pd.DataFrame({
@@ -403,16 +356,12 @@ def pretrain_speaker(
             "steps": val_steps,
             "val_losses": val_losses
         })
-        # df_val_out.to_csv(val_csv_out + "epoch_" + str(epoch) + ".csv", index=False)
+        df_val_out.to_csv(val_csv_out + "epoch_" + str(epoch) + ".csv", index=False)
 
         metrics_out = pd.DataFrame({
             "steps": eval_steps,
             "structural_drift_true": structural_drifts_true,
             "structural_drift_pred": structural_drifts,
-            # "semantic_drifts_true": semantic_drifts_true,
-            # "semantic_drifts_pred": semantic_drifts,      
-            # "discrete_overlaps": discrete_overlaps,
-            # "continuous_overlaps": cont_overlaps,
         })
         metrics_out.to_csv(csv_metrics + "epoch_" + str(epoch) + ".csv", index=False)
 
